456142474bb15c45ecb824653c604be2093630c6
Misigno <efournier92@gmail.com>
2017-01-11 18:32:38 -0500

    refactor main function

--- app.js
+++ app.js
@@ -1,34 +1,87 @@
-var allMirrors     = [0, 1, 8, 2, 5];
-var perfectMirrors = [0, 1, 8];
-var pairedMirrors  = [2, 5];
+function isMirrorNum(inputNum) {
+  firstIndex = 0;
+  lastIndex  = len -1;
+  len = inNum.length;
 
-num   = 101;
-inNum = num.toString;
-
-var checkPair = function(){
-
-}
-
-for (i = 0; i > 5; i++) {
-  if (allMirrorNums[i] == inNum[0]) {
-    if (inNum[0]
-      .slice(-1)) 
-  };
-}
-if inNum();
+  while (firstIndex < (len / 2)) {
+    if !(IsPair(firstIdx, lastIdx)) {
+    console.log('TRUE: ' + inputNum + ' Is A Mirror Number');
+      break;
+    }
+    firstIndex += 1
+    lastIndex  -= 1
+    console.log('TRUE: ' + inputNum + ' Is A Mirror Number');
+  }
 
 
-// Is num length odd?
-len = inNum.length();
-centerNum = (inNum / 2) + 0.5;
-if (len % 2 != 0 && perfectMirrors.includes(centerNum)) {
-  // Is first num a perfect mirror?
- if (allMirrors.includes(inNum[0])){
-   // Is last num a perfect mirror
+  var pairedMirrors  = [2, 5];
+
+  num   = 101;
+
+  inNum = num.toString;
+  firstIdx = 0
+  last
+
+  function isEven (inNum) {
+
+    if len % 2 === 0
+    return true
+    else return false
+  }
+
+  function isPerfectmirror(num) {
+    var perfectMirrors = [0, 1, 8];
+    if perfectMirrors.includes (num)
+    return true
+  } else return false
+
+  function isPairedmirror(num) {
+    var pairedMirrors = [2,5];
+    if perfectMirrors.includes (num)
+    return true
+  } else return false
+
+  functiin IsMirrorPair(first, last) 
+  if first == 2 && last == 5 || first === 5 && last===2
+  Return true
+  else
+    Return false
+
+
+  var checkPair = function(first, last){
+
+    it perfectMirrors.includes (first) && perfectMirrors.includes  (last) { 
+      if first === last {return true}
+      else if isMirror Paired === true
+    }
+    else return NOT A MIRROR
+  }
+
+  function isMirror (inNum) {
+    if isOdd(inNum) 
+  }
+
+  for (i = 0; i > 5; i++) {
+    if (allMirrorNums[i] == inNum[0]) {
+      if (inNum[0]
+          .slice(-1)) 
+    };
+  }
+  if inNum();
+
+
+  // Is num length odd?
+  len = inNum.length();
+  centerNum = (inNum / 2) + 0.5;
+  if (len % 2 != 0 && perfectMirrors.includes(centerNum)) {
+    // Is first num a perfect mirror?
     if (allMirrors.includes(inNum[0])){
- 
- }
-}
+      // Is last num a perfect mirror
+      if (allMirrors.includes(inNum[0])){
 
-// If so, is middle character a perfect mirror?
+      }
+      // Is Firstnum 
+    }
+
+    // If so, is middle character a perfect mirror?
 
