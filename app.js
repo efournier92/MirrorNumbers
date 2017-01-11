0341dd46fa1c14e2a473f30a21c7d34ab9629a18
Misigno <efournier92@gmail.com>
2017-01-11 18:23:27 -0500

    start writing pseudocode

---
+++ app.js
@@ -0,0 +1,34 @@
+var allMirrors     = [0, 1, 8, 2, 5];
+var perfectMirrors = [0, 1, 8];
+var pairedMirrors  = [2, 5];
+
+num   = 101;
+inNum = num.toString;
+
+var checkPair = function(){
+
+}
+
+for (i = 0; i > 5; i++) {
+  if (allMirrorNums[i] == inNum[0]) {
+    if (inNum[0]
+      .slice(-1)) 
+  };
+}
+if inNum();
+
+
+// Is num length odd?
+len = inNum.length();
+centerNum = (inNum / 2) + 0.5;
+if (len % 2 != 0 && perfectMirrors.includes(centerNum)) {
+  // Is first num a perfect mirror?
+ if (allMirrors.includes(inNum[0])){
+   // Is last num a perfect mirror
+    if (allMirrors.includes(inNum[0])){
+ 
+ }
+}
+
+// If so, is middle character a perfect mirror?
+
