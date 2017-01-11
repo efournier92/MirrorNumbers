a5e00628899f827c757a7dcc2d83d3f114c81166
Misigno <efournier92@gmail.com>
2017-01-11 18:58:58 -0500

    fix ! bug

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
 function isMirrorNum(inputNum) {
   const perfectMirrors = [0, 1, 8];
-  inNum = num.toString;
+  inputNum = inputNum.toString;
+  inputNumLength = inNum.length;
   firstIndex = 0;
-  lastIndex  = len -1;
-  len = inNum.length;
+  lastIndex = len - 1;
 
   while (firstIndex < (len / 2)) {
-    if !(isPair(firstIdx, lastIdx)) {
-    console.log('TRUE: ' + inputNum + ' Is A Mirror Number');
+    if (!(isPair(firstIdx, lastIdx))) {
+      console.log('FALSE: ' + inputNum + ' is NOT a mirror number.');
       break;
     }
     firstIndex += 1
     lastIndex  -= 1
-    console.log('TRUE: ' + inputNum + ' Is A Mirror Number');
+    console.log('TRUE: ' + inputNum + ' IS a mirror number.');
   }
+}
 
-  function isOdd(num) {
-    if (num % 2 == 0) {
+function checkPair(first, last) {
+  if perfectMirrors.includes(first) && perfectMirrors.includes(last) { 
+    if first === last {
       return true;
-    } 
-    else {
-      return false;
-    }
-  }
-
-  function isMirrorChar(num) {
-    if perfectMirrors.includes(num) {
+    } else if (is25Pair) {
       return true;
     } else {
       return false;
     }
+  }
+}
 
-    function is25Pair(first, last) {
-      if (first == 2 && last == 5 || first == 5 && last == 2) {
-        return true;
-      } else {
-        return false;
-      }
-    }
+function isOdd(num) {
+  if (num % 2 == 0) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
 
-    function checkPair(first, last) {
-      if perfectMirrors.includes(first) && perfectMirrors.includes(last) { 
-        if first === last {
-          return true;
-        } else if (is25Pair) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    }
+function isMirrorChar(num) {
+  if perfectMirrors.includes(num) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function is25Pair(first, last) {
+  if (first == 2 && last == 5 || first == 5 && last == 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
