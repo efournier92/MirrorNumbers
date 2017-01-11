cc02de5fbbbeb49e08b158d76873c06742576134
Misigno <efournier92@gmail.com>
2017-01-11 18:48:51 -0500

    refactor all functions

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function isMirrorNum(inputNum) {
+  const perfectMirrors = [0, 1, 8];
+  inNum = num.toString;
   firstIndex = 0;
   lastIndex  = len -1;
   len = inNum.length;
 
   while (firstIndex < (len / 2)) {
-    if !(IsPair(firstIdx, lastIdx)) {
+    if !(isPair(firstIdx, lastIdx)) {
     console.log('TRUE: ' + inputNum + ' Is A Mirror Number');
       break;
     }
@@ -13,43 +15,30 @@ function isMirrorNum(inputNum) {
     console.log('TRUE: ' + inputNum + ' Is A Mirror Number');
   }
 
-
-  var pairedMirrors  = [2, 5];
-
-  num   = 101;
-
-  inNum = num.toString;
-  firstIdx = 0
-  last
-
-  function isEven (inNum) {
-
-    if len % 2 === 0
-    return true
-    else return false
+  function isOdd(num) {
+    if (num % 2 == 0) {
+      return true;
+    } 
+    else {
+      return false;
+    }
   }
 
-  function isPerfectmirror(num) {
-    var perfectMirrors = [0, 1, 8];
-    if perfectMirrors.includes (num)
-    return true
-  } else return false
+  function isMirrorChar(num) {
+    if perfectMirrors.includes(num) {
+      return true;
+    } else {
+      return false;
+    }
 
-  function isPairedmirror(num) {
-    var pairedMirrors = [2,5];
-    if perfectMirrors.includes (num)
-    return true
-  } else return false
+  function is25Pair(first, last) {
+  if (first == 2 && last == 5 || first == 5 && last == 2) {
+    return true;
+  } else {
+    return false;
+  }
 
-  functiin IsMirrorPair(first, last) 
-  if first == 2 && last == 5 || first === 5 && last===2
-  Return true
-  else
-    Return false
-
-
-  var checkPair = function(first, last){
-
+  var checkPair = function(first, last) {
     it perfectMirrors.includes (first) && perfectMirrors.includes  (last) { 
       if first === last {return true}
       else if isMirror Paired === true
