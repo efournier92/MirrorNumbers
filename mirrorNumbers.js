bab8af7e72b70c8d0701beb54e6f75623d0802b8
Misigno <efournier92@gmail.com>
2017-01-11 22:45:44 -0500

    add comments

--- mirrorNumbers.js
+++ mirrorNumbers.js
@@ -5,20 +5,28 @@ function isMirrorNum(input) {
   var lastIndex = inputNumLength - 1;
 
   while (firstIndex < (inputNumLength / 2)) {
+    // pair the first digit with the last digit
     if (!(isPair(inputNum[firstIndex], inputNum[lastIndex]))) {
+      // if any pair doesn't match, input is not a valid mirror number
       return('FALSE: ' + input + ' is NOT a mirror number.');
     }
+    // shift focus to next digit pair, moving inward
     firstIndex += 1;
     lastIndex -= 1;
+    // repeat until no digits remain
   }
   return('TRUE: ' + input + ' IS a mirror number.');
 }
 
 function isPair(first, last) {
-  if (isMirrorChar(first) 
-      && isMirrorChar(last)
+  // the only mirror digits are 0, 1, & 8 
+  if (isMirrorDigit(first) 
+      && isMirrorDigit(last)
+      // corresponding mirror digits must be equal
       && first === last) { 
     return true;
+    // 2 & 5 are a mirror pair
+    // if a digit is 2, the corresponding digit must be 5, and vice versa 
   } else if (is25Pair(first, last)) {
     return true;
   } else {
@@ -26,8 +34,8 @@ function isPair(first, last) {
   }
 }
 
-function isMirrorChar(num) {
-  const perfectMirrors = ["0", "1", "8"];
+function isMirrorDigit(num) {
+  var perfectMirrors = ["0", "1", "8"];
   if (perfectMirrors.includes(num)) {
     return true;
   } else {
